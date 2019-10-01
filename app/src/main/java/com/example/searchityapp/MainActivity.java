922c1c89da3d659dee193bf63ff8fc82d33e65b3
Aguilera Monroy <antonio.aguilerimon@gmail.com>
2019-09-30 19:40:47 -0500

    Actividad de Preregistro, relacion entre actividades, diseño de actividades e iconos de la aplicacion

--- app/src/main/java/com/example/searchityapp/MainActivity.java
+++ app/src/main/java/com/example/searchityapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.searchityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Metodo hacia preregistro
+    public void preregistro(View view) {
+        Intent preregistro = new Intent(this, ActividadPreregistro.class);
+        startActivity(preregistro);
     }
 }
